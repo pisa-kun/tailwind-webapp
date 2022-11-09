bb6a163414db7902a0a2d3bd09512d2900a0302a
pisa <pisa0401mst@gmail.com>
2022-11-10 05:35:00 +0900

    skill set

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         "selected-text": "#A3A3FF",
         theme: "#783b4a",
         secondary: "#9e888e",
+        badge: "#4f3e43",
+        golang: "#4285F4",
+        aws: "#FF9900",
+        windows: "#0078D7",
       },
     },
     fontFamily: {
